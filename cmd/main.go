17de41ea12a431c12bae8b80b56e9e406bff5b07
Shocky <shocky92@live.ru>
2022-02-12 16:10:41 +0600

    add prompt select of operands

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,44 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
+
+	"github.com/manifoldco/promptui"
 )
 
 const (
-	OperationAddition = "+"
-	OperationSubtract = "-"
-	OperationMultiply = "*"
-	OperationDivide   = "/"
-	OperationPower    = "^"
+	OperationAddition = "Сложение +"
+	OperationSubtract = "Вычитание -"
+	OperationMultiply = "Умножение *"
+	OperationDivide   = "Деление /"
+	OperationPower    = "Возведение в степень ^"
 )
 
-func main() {
-	var firstOperand, secondOperand, result float64
-	var operator string
+func getOperator() (string, error) {
+	prompt := promptui.Select{
+		Label: "Выберите оператор:",
+		Items: []string{
+			OperationAddition,
+			OperationSubtract,
+			OperationMultiply,
+			OperationDivide,
+			OperationPower,
+		},
+	}
 
-	fmt.Println("Введите оператор (+ - * / ^), первый и второй операнд:")
-	fmt.Scan(&operator, &firstOperand, &secondOperand)
+	_, operator, err := prompt.Run()
+
+	if err != nil {
+		return "", errors.New("Ошибка! Неверно выбран оператор.")
+	}
+
+	return operator, nil
+}
+
+func calculate(operator string, firstOperand float64, secondOperand float64) (float64, error) {
+	var result float64
 
 	switch operator {
 	case OperationAddition:
@@ -29,16 +49,28 @@ func main() {
 		result = firstOperand * secondOperand
 	case OperationDivide:
 		if secondOperand == 0 {
-			fmt.Println("Деление на ноль невозможно")
-			return
+			return result, errors.New("Деление на ноль невозможно")
 		}
 		result = firstOperand / secondOperand
 	case OperationPower:
 		result = math.Pow(firstOperand, secondOperand)
 	default:
-		fmt.Println("Неверно выбран оператор")
-		return
+		return result, errors.New("Неверно выбран оператор")
 	}
+
+	return result, nil
+}
+
+func main() {
+	var operator string
+	var firstOperand, secondOperand, result float64
+
+	operator, _ = getOperator()
+
+	fmt.Println("Введите первый и второй операнд:")
+	fmt.Scan(&firstOperand, &secondOperand)
+
+	result, _ = calculate(operator, firstOperand, secondOperand)
 
 	fmt.Println("Результат: ", result)
 }
