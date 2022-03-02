192f75979c8e6c3b5c88e83e9559f23e57bcee73
Shocky <shocky92@live.ru>
2022-03-02 19:42:00 +0600

    add config reader function

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"config"
+	"fmt"
+)
+
+func main() {
+	cfgPath, err := config.ParseFlags()
+	if err != nil {
+		panic(err)
+	}
+
+	cfg, err := config.NewConfig(cfgPath)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(cfg.Port)
+	fmt.Println(cfg.DBUrl)
+	fmt.Println(cfg.JaegerUrl)
+	fmt.Println(cfg.SentryUrl)
+	fmt.Println(cfg.KafkaBroker)
+	fmt.Println(cfg.SomeAppId)
+	fmt.Println(cfg.SomeAppKey)
+}
--- config/config.go
+++ config/config.go
@@ -1,5 +1,13 @@
 package config
 
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
 type config struct {
 	Port        int    `yaml:"port"`
 	DBUrl       string `yaml:"db_url"`
@@ -8,4 +16,48 @@ type config struct {
 	KafkaBroker string `yaml:"kafka_broker"`
 	SomeAppId   string `yaml:"some_app_id"`
 	SomeAppKey  string `yaml:"some_app_key"`
+}
+
+func NewConfig(configPath string) (*config, error) {
+	config := &config{}
+
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+
+	if err := decoder.Decode(config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func ValidatePath(path string) error {
+	stat, err := os.Stat(path)
+
+	if err != nil {
+		return err
+	}
+
+	if stat.IsDir() {
+		return fmt.Errorf("'%s' is a directory", path)
+	}
+	return nil
+}
+
+func ParseFlags() (string, error) {
+	var configPath string
+
+	flag.StringVar(&configPath, "config", "./conf.yaml", "path to config file")
+	flag.Parse()
+
+	if err := ValidatePath(configPath); err != nil {
+		return "", err
+	}
+
+	return configPath, nil
 }
